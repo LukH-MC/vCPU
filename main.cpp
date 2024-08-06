fa71d42b1daabd014d6de2fbe14998340cb4fd41
LukH-MC <lukas.balkhausen@gmail.com>
2024-08-06 12:24:37 +0200

    Added memory fixed define error Added readme.md

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <stdlib.h>
+#include <stdio.h>
+using Byte = unsigned char;
+using Word = unsigned short;
+
+using u32 = unsigned int;
+
+struct Mem {
+    static constexpr u32 MAX_MEM = 1024 * 64;
+    Byte Data[MAX_MEM];
+
+    void Initialise() {
+        for(u32 i = 0; i < MAX_MEM; i++) {
+            Data[i] = 0;
+        }
+    }
+};
+
+
 
 struct CPU {
-    using Byte = unsigned char;
-    using Word = unsigned short;
-
     Word PC; //Program Counter
     Byte SP; //Stack pointer
 
@@ -17,17 +32,19 @@ struct CPU {
     Byte V : 1; //status flag
     Byte N : 1; //status flag
 
-    void Reset() {
+    void Reset(Mem& memory) {
         PC = 0xFFFC; //reset vector
         SP = 0x0100;
         C = Z = I = D = B = V = N = 0;
         A = X = Y = 0;
+        memory.Initialise();
     }
 };
 
 
 int main() {
+    Mem mem;
     CPU cpu;
-    cpu.Reset();
+    cpu.Reset(mem);
     return 0;
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+My personal attempt at emulating the c6402 CPU.
+
+This is not the most realistic emulation (like obvious in CPU.Reset())
